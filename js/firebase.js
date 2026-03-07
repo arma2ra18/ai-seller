8d1f90b5f7b8b29f7735723f3f932aac2d4e1128
arma2ra18 <masterkrd23.ru@gmail.com>
2026-03-07 18:09:52 +0300

    Update firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
+import { getFunctions } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-functions.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAllWDGr8TKYeqtjgpM9EWWf5hqSfPJqoI", // ЗАМЕНИ НА СВОИ
+    authDomain: "ai-seller-prod-4c0c9.firebaseapp.com",
+    projectId: "ai-seller-prod-4c0c9",
+    storageBucket: "ai-seller-prod-4c0c9.firebasestorage.app",
+    messagingSenderId: "951491114415",
+    appId: "1:951491114415:web:de94518305c12d56602b5a"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const functions = getFunctions(app);
+export const storage = getStorage(app);
