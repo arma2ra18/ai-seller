3e9c9b9b9c96951f8a0bcb7681d04d0a52334044
arma2ra18 <masterkrd23.ru@gmail.com>
2026-03-14 16:53:36 +0300

    1

--- js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-
-const supabaseUrl = 'https://sqvmbirikyrxldqzvthl.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxdm1iaXJpa3lyeGxkcXp2dGhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMTcwNTQsImV4cCI6MjA4ODg5MzA1NH0.N2n8xPtGSL9JPs55-Z5sIR4L3yu8WU6WEMwZ6TqrvuQ'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
